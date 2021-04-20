736a3a05cb8968468845bdc51449a67093de9358
shan hsu <ppearl0329tw@gmail.com>
2021-04-20 15:26:00 +0800

    deleted

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,9 +1,13 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function(){
   document.querySelectorAll('li').forEach(function(el){
-el.addEventListener('click', function(){
-  el.classList.toggle('checked')
-})
+    el.addEventListener('click', function(){
+    el.classList.toggle('checked')
+    })
   })  
-  
+  document.querySelectorAll('.close').forEach(function(de){
+    de.addEventListener('click', function(){
+    de.parentNode.remove()
+    })
   })
+})
